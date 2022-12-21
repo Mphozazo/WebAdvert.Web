62039dccbfa61f949c0e0fb8f559e48ae8b3226f
Walter Heisi <Mphozazo@gmail.com>
2022-12-21 08:35:46 +0200

    First code

---
+++ WeWebAdvert.Web/Controllers/Accounts.cs
@@ -0,0 +1,60 @@
+﻿using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeWebAdvert.Web.Models.Accounts;
+
+namespace WeWebAdvert.Web.Controllers
+{
+    public class Accounts : Controller
+    {
+
+        readonly SignInManager<CognitoUser> SignInManager;
+        readonly UserManager<CognitoUser> UserManager;
+        readonly CognitoUserPool UserPool;
+        public Accounts(SignInManager<CognitoUser> signInManager , UserManager<CognitoUser> userManager , CognitoUserPool userPool )
+        {
+            SignInManager = signInManager;
+            UserManager = userManager;
+            UserPool = userPool;
+
+        }
+        public async Task<IActionResult> SignUp()
+        {
+            var user = new Signup();
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(Signup model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = UserPool.GetUser(model.Email);
+
+                if (user != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with email address already exists.");
+
+                    return View(model);
+                }
+                user.Attributes.Add(CognitoAttribute.Name.ToString(), model.Email);
+                var  createdUser  = await UserManager.CreateAsync(user, model.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    RedirectToAction("Confirm"); 
+                }
+            }
+              
+
+            return View(); 
+        }
+
+    }
+}
---
+++ WeWebAdvert.Web/Models/Accounts/Signup.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeWebAdvert.Web.Models.Accounts
+{
+    public class Signup
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(6,ErrorMessage ="Minimum of 6 in lenght.")]
+        [Display(Name =("Password"))]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Password and Confirmation does not match.")]
+        [Display(Name = ("Confirm Password"))]
+        public string ConfirmPassword { get; set; }
+    }
+}
